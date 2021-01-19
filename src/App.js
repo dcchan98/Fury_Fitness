90142165e82c640cbb9d93e8e00afe9323224309
Sean Chan <dcchan98@gmail.com>
2021-01-19 19:05:32 +0800

    Add bootstrap to project

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ import {
   Route,
   Link
 } from "react-router-dom";
-    // <div className="App">
-    //  <HomePage />
-    //  <ShopPage />
-    //  <TrackerPage />
-    //  <AuthenticationPage />
-    // </div>   <Router>
 
 function App() {
   return (
@@ -37,13 +31,6 @@ function App() {
 
         <hr />
 
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
             <HomePage />
