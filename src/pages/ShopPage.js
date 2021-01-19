260d15fc0ee0f99993c506b19d007fb8e00f434f
Sean Chan <dcchan98@gmail.com>
2021-01-19 19:38:13 +0800

    add resources directory

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,17 +1,34 @@
-import React from 'react'
-import ShopCard from '../components/ShopCard'
+/** @format */
+
+import React from "react";
+import ShopCard from "../components/ShopCard";
 
 export default function ShopPage() {
-  return (
-    <div className= "container">
+	return (
+		<div className='container'>
+			<div class='row form-group'>
+				<div class='col-sm'>
+					<ShopCard />
+				</div>
+				<div class='col-sm'>
+					<ShopCard />
+				</div>{" "}
+				<div class='col-sm'>
+					<ShopCard />
+				</div>
+			</div>
 
-    
-      <ShopCard/>
-      <ShopCard/>
-      <ShopCard/>
-      <ShopCard/>
-      <ShopCard/>
-
-    </div>
-  )
+			<div class='row'>
+				<div class='col-sm'>
+					<ShopCard />
+				</div>
+				<div class='col-sm'>
+					<ShopCard />
+				</div>{" "}
+				<div class='col-sm'>
+					<ShopCard />
+				</div>
+			</div>
+		</div>
+	);
 }
