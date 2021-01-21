eaf5a4a32cd59a4daf215febd6707d8814b67f40
Sean Chan <dcchan98@gmail.com>
2021-01-21 18:05:14 +0800

    Refactor app to class based component

--- src/App.js
+++ src/App.js
@@ -12,70 +12,71 @@ import { Navbar, Nav } from "react-bootstrap";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-	const [currentUser, setCurrentUser] = useState(null);
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { currentUser: null };
+	}
 
-	useEffect(
-		() => {
-			auth.onAuthStateChanged(
-				user=>{
-					setCurrentUser(user)
-					console.log("Current User in App state")
-					console.log(currentUser)
-				}
-			);
-		}
-	, [currentUser]  );
-	
-	return (
-		<Router>
-			<div>
-				<div className='container'>
-					<Navbar bg='light' expand='lg'>
-						<Navbar.Toggle aria-controls='basic-navbar-nav' />
-						<Navbar.Collapse id='basic-navbar-nav'>
-							<Nav className='mr-auto'>
-								<Nav.Link>
-									<Link to='/'>Fury Fitness</Link>
-								</Nav.Link>
-							</Nav>
-							<Nav className='mr-auto'>
-								<Nav.Link>
-									<Link to='/authenticate'>Log In</Link>
-								</Nav.Link>
-							</Nav>
-							<Nav className='mr-auto'>
-								<Nav.Link>
-									<Link to='/shop'>Shop</Link>
-								</Nav.Link>
-							</Nav>
-							<Nav className='mr-auto'>
-								<Nav.Link>
-									<Link to='/tracker'>Tracker</Link>
-								</Nav.Link>
-							</Nav>
-						</Navbar.Collapse>
-					</Navbar>
+	componentDidMount() {
+		auth.onAuthStateChanged((user) => {
+			this.setState({ currentUser: user });
+			console.log("Current User in App state");
+			console.log(this.state.currentUser);
+		});
+	}
+
+	render() {
+		return (
+			<Router>
+				<div>
+					<div className='container'>
+						<Navbar bg='light' expand='lg'>
+							<Navbar.Toggle aria-controls='basic-navbar-nav' />
+							<Navbar.Collapse id='basic-navbar-nav'>
+								<Nav className='mr-auto'>
+									<Nav.Link>
+										<Link to='/'>Fury Fitness</Link>
+									</Nav.Link>
+								</Nav>
+								<Nav className='mr-auto'>
+									<Nav.Link>
+										<Link to='/authenticate'>Log In</Link>
+									</Nav.Link>
+								</Nav>
+								<Nav className='mr-auto'>
+									<Nav.Link>
+										<Link to='/shop'>Shop</Link>
+									</Nav.Link>
+								</Nav>
+								<Nav className='mr-auto'>
+									<Nav.Link>
+										<Link to='/tracker'>Tracker</Link>
+									</Nav.Link>
+								</Nav>
+							</Navbar.Collapse>
+						</Navbar>
+					</div>
+					<hr />
+
+					<Switch>
+						<Route exact path='/'>
+							<HomePage />
+						</Route>
+						<Route path='/authenticate'>
+							<AuthenticationPage />
+						</Route>
+						<Route path='/shop'>
+							<ShopPage />
+						</Route>
+						<Route path='/Tracker'>
+							<TrackerPage />
+						</Route>
+					</Switch>
 				</div>
-				<hr />
-
-				<Switch>
-					<Route exact path='/'>
-						<HomePage />
-					</Route>
-					<Route path='/authenticate'>
-						<AuthenticationPage />
-					</Route>
-					<Route path='/shop'>
-						<ShopPage />
-					</Route>
-					<Route path='/Tracker'>
-						<TrackerPage />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
-	);
+			</Router>
+		);
+	}
 }
 
 export default App;
