1024140e391056b851c9b4a08bebdd13d19e18bb
Sean Chan <dcchan98@gmail.com>
2021-01-22 15:36:06 +0800

    get button from firestore for debugging and reference purposes

--- src/firebase/firebase.storage.js
+++ src/firebase/firebase.storage.js
@@ -7,3 +7,10 @@ firestore.settings({
   timestampsInSnapshots: true
 })
 
+
+// get users for debugging
+export const getUsers =  () =>{
+  firestore.collection('users').get().then(ss=>
+    ss.docs.forEach(doc=>console.log(doc.data()))
+    )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import {getUsers} from "../firebase/firebase.storage"
 
 export default function HomePage() {
   return (
     <div>
+    <button onClick={getUsers}>Users</button>
       Home
     </div>
   )
