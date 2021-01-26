0ed198199ff907138e59aa05588e984622d2d12e
Sean Chan <dcchan98@gmail.com>
2021-01-26 15:09:33 +0800

    create store, apply middleware, link store

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import App from "./App";
 import { Provider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import store from "./redux/store"
 
 
 
 
 ReactDOM.render(
-	<Provider>
+	<Provider store ={store} >
 		<App />
 	</Provider>,
 	document.getElementById("root")
