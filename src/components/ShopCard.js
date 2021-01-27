8129242d450ed703b310f145b1da46bba1980acc
Sean Chan <dcchan98@gmail.com>
2021-01-27 18:54:22 +0800

    change buy button based on login state

--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -3,13 +3,14 @@
 import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { connect } from "react-redux";
 
 import StripeButton from "./StripeButton";
 
 
 const PRICE = 49.99 // cost of hiring training is  sgd 49.99
 
-export default class ShopCard extends Component {
+class ShopCard extends Component {
 	render() {
 		return (
 			<div className="col-sm-12 col-md-6 col-lg-4 form-group">
@@ -21,12 +22,20 @@ export default class ShopCard extends Component {
 						<Card.Text>
             {this.props.hero.description}
 						</Card.Text>
-
-						<Button variant="secondary" disabled>Log In To Buy</Button>
-						<StripeButton price ={PRICE}/>
+						{this.props.currentUser.displayName == null?<Button variant="secondary" disabled>Log In To Buy</Button>:<StripeButton price ={PRICE}/>}
+						
 					</Card.Body>
 				</Card>
 			</div>
 		);
 	}
 }
+
+const mapStateToProps = (state) => {
+	if (state.user.currentUser==null){
+		return {currentUser: {displayName:null}}
+	}
+	return {currentUser: state.user.currentUser.user}
+};
+
+export default connect(mapStateToProps)(ShopCard);
