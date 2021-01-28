31d5ae49293a467f639364b14717458c6cbc9b7f
Sean Chan <dcchan98@gmail.com>
2021-01-28 23:47:00 +0800

    test out fitness health calculation library

--- src/pages/RecommenderPage.js
+++ src/pages/RecommenderPage.js
@@ -5,7 +5,15 @@ import FoodInput from "../components/FoodInput";
 import PercentageChart from "../components/PercentageChart";
 import Jumbotron from "react-bootstrap/Jumbotron";
 
+// test out
+import calculate from "fitness-health-calculations"
+
 import { connect } from "react-redux";
+
+const myCaloricNeeds = calculate.caloricNeeds('male', 23, 175, 80, 'moderate', 'reduction', 'agressive');
+
+const tdee = calculate.tdee('male', 23, 175, 80, 'moderate');
+const bmr = calculate.bmr('male', 23, 175, 80);
 
 function RecommenderPage(props) {
 	if (props.currentUser != null) {
@@ -15,7 +23,13 @@ function RecommenderPage(props) {
 					<h1>Recommender</h1>
 					<br></br>
 					<p>Fill in the various input to calculate your caloric needs</p>
-					<p></p>
+					{/* Fill in code here */}
+					<p>	BMR		{		bmr	}	</p>
+					<p>	TDEE		{		tdee	}	</p>
+					<p>	Caloric		{		myCaloricNeeds	}	</p>
+
+			
+					{/* Fill in code here */}
 				</Jumbotron>
 			</div>
 		);
